c5bcd72f96b725ac207721a5f07b571cff376cf4
dzaton <dzaton96@gmail.com>
2022-11-16 22:46:05 +0100

    Initial commit

---
+++ universityApiBackend/DataAccess/UniversityDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using UniversityApiBackend.Models.DataModels;
+
+namespace universityApiBackend.DataAccess
+{
+    public class UniversityDbContext: DbContext
+    {
+        public UniversityDbContext(DbContextOptions<UniversityDbContext> options) : base(options) { }
+    
+    // TODO: Add DbSets (Tables of our Data base)
+        public DbSet<User>? Users { get; set; }
+
+    }
+}
---
+++ universityApiBackend/Models/DataModels/Course.cs
@@ -0,0 +1,26 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace UniversityApiBackend.Models.DataModels
+{
+    public class Course:BaseEntity
+    {
+        [Required, StringLength(50)]
+        public string Name { get; set; } = string.Empty;
+        [Required, StringLength(280)]
+        public string ShortDescription { get; set; } = string.Empty;
+        [Required]
+        public string LongDescription { get; set; } = string.Empty;
+        [Required]
+        public string TargetAudience { get; set; } = string.Empty;
+        [Required]
+        public string Objectives { get; set; } = string.Empty;
+        [Required]
+        public string Requirements { get; set; } = string.Empty;
+        public enum Level 
+        {
+            Basic,
+            Intermediate,
+            Advance
+        }
+    }
+}
